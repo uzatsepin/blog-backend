811bfe45aa7fb00ce052516b46ba059355ee4e26
Andrii Zatsepin <uzatsepin@gmail.com>
2023-08-11 18:53:57 +0300

    fixed creating post

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -70,7 +70,7 @@ export const createPost = async (req, res) => {
     try {
         const doc = new PostModel({
             title: req.body.title,
-            text: req.body.title,
+            text: req.body.text,
             imageUrl: req.body.imageUrl,
             tags: req.body.tags,
             user: req.userId,
